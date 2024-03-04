93d3ded93788a04ceb95b905f906e05344c7bd6a
sudarshankapali <sudarshankapalisk@gmail.com>
2024-03-04 22:58:54 +0545

    day-2

---
+++ Day-2/index.php
@@ -0,0 +1,20 @@
+<?php
+$status = "login";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action='/' method='post'>
+        <button type='submit'>Login</button>
+    </form>
+    <form action='/' method='post'>
+        <button type='submit'>Logout</button>
+    </form>
+</body>
+</html>
