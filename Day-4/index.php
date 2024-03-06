3e399806728bb3d26aff41b202b76b2097d97d5e
sudarshankapali <sudarshankapalisk@gmail.com>
2024-03-06 23:01:33 +0545

    Fixing errors

--- Day-4/index.php
+++ Day-4/index.php
@@ -11,24 +11,26 @@ $conn = mysqli_connect($hostname, $username, $password, $database_name);
 
 echo " connection success ✅";
 // fetch the data from the backend 
-$sql = "select * from Food_Table";
-$result = mysqli_query($conn, $sql);
+// $sql = "select * from Food_Table";
+// $result = mysqli_query($conn, $sql);
 // echo "<pre>";
 // var_dump($result);
 // echo "</pre>";
 
 $sql = "select * from Food_Table";
+$sql1 = "insert into Food_Table (Name) values ('Test_Data3')";
+mysqli_query($conn, $sql1);
+echo "<br>data inserted sucessfully";
 $result = mysqli_query($conn, $sql);
 $rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 ?>
 
 <ul>
-  <?php
-  foreach ($rows as $value) {
+<?php
+foreach ($rows as $value) {
     echo "<li>{$value['ID']} {$value['Name']} {$value['created_at']}</li>";
-  }
+}
 
-  ?>
-</ul>
 ?>
+</ul>
