f6bd2e38a0496b1af41c50af9cc18f3ca6f48cfc
sudarshankapali <sudarshankapalisk@gmail.com>
2024-03-11 22:51:12 +0545

    url shoterning

--- Day-6/add.php
+++ Day-6/add.php
@@ -1,12 +1,11 @@
 <?php
 require 'connect.php';
 if(isset($_POST['url'])){
-    $shorturl = "localhost-3";
+    $shorturl = uniqid('r-');
     $longUrl = $_POST['url'];
     echo "$shorturl";
     $insert = "insert into url(long_url,short_url ) values ('$longUrl','$shorturl')";
     $run = mysqli_query($conn,$insert);
-    // echo "sucess";
     header('Location: /');
 }
 ?>
--- Day-6/index.php
+++ Day-6/index.php
@@ -1,9 +1,20 @@
+<?php
+require 'connect.php';
+$sql = "select * from url";
+$result = mysqli_query($conn, $sql);
+$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        th,td{
+            border: 2px solid black;
+        }
+    </style>
 </head>
 <body>
     <form action='add.php' method='post'>
@@ -11,5 +22,28 @@
         <br>
         <button type='submit'>Generate</button>
     </form>
+    <section>
+        <h1>perious generated links</h1>
+        <table>
+            <thead>
+                <tr>
+                    <thead>Long URL</thead>
+                    <thead>Short URL</thead>
+                </tr>
+                <tbody>
+        <?php foreach ($rows as $key => $value) : ?>
+        <tr>
+            <!-- Just to display the links, feel free to make your own style changes -->
+            <td><a href="/q/?r=<?= $value['short_url'] ?>">
+                <?= $_SERVER['HTTP_HOST'] ?>/q/?r=<?= $value['short_url'] ?></a>
+
+            </td>
+            <td> <span> <?= $value['long_url'] ?></span></td>
+        </tr>
+        <?php endforeach ?>
+        </tbody>
+            </thead>
+        </table>
+    </section>
 </body>
 </html>
