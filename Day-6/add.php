b1e5e15578f7ed36fb1e0c7b5bc7bf162dd2d8a4
sudarshankapali <sudarshankapalisk@gmail.com>
2024-03-11 09:23:40 +0545

    s

--- Day-6/add.php
+++ Day-6/add.php
@@ -1,7 +1,11 @@
 <?php
 require 'connect.php';
 if(isset($_POST['url'])){
-    //echo "{$_POST['url']}";
-    $insert = '';
+    $shorturl = "localhost/"  + rand(1,10000);
+    $longUrl = $_POST['url'];
+    echo "$shorturl";
+    $insert = "insert into url(long_url,short_url ) values ('$longUrl','$shorturl')";
+    $run = mysqli_query($conn,$insert);
+    echo "sucess";
 }
 ?>
