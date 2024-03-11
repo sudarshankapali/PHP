47341590f2053d144d5e62b52dce4bdfb7172a98
sudarshankapali <sudarshankapalisk@gmail.com>
2024-03-11 09:35:08 +0545

    insert done

--- Day-6/add.php
+++ Day-6/add.php
@@ -1,11 +1,12 @@
 <?php
 require 'connect.php';
 if(isset($_POST['url'])){
-    $shorturl = "localhost/"  + rand(1,10000);
+    $shorturl = "localhost-3";
     $longUrl = $_POST['url'];
     echo "$shorturl";
     $insert = "insert into url(long_url,short_url ) values ('$longUrl','$shorturl')";
     $run = mysqli_query($conn,$insert);
-    echo "sucess";
+    // echo "sucess";
+    header('Location: /');
 }
 ?>
